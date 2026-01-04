18f29b307ecdfa0970021929ffb1b81a3ddde7d1
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-05 00:00:56 +1000

    [refactor]

--- src/route/auth/index.ts
+++ src/route/auth/index.ts
@@ -2,10 +2,10 @@ import { Application } from 'express'
 import userRoute from 'route/auth/user'
 import authRoute from 'route/auth/auth'
 import { MODULE } from 'config/dir'
-import { connect } from 'utility'
+import { combine } from 'utility'
 
 
 export const linkAuthRoute = (app: Application, apiVer: string) => {
-    app.use(connect(apiVer, MODULE.AUTH), authRoute)
-    app.use(connect(apiVer, MODULE.AUTH, MODULE.USER), userRoute)
+    app.use(combine(apiVer, MODULE.AUTH), authRoute)
+    app.use(combine(apiVer, MODULE.AUTH, MODULE.USER), userRoute)
 }
