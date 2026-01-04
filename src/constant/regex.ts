be4c959065dd5fb24cca8d909c22d38d30284582
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-05 00:02:11 +1000

    date regex added

--- src/constant/regex.ts
+++ src/constant/regex.ts
@@ -3,6 +3,7 @@ export const REGEX = {
   PASSWORD: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
   PHONE   : /^[0-9]{10,11}$/,
   URL     : /^(http|https):\/\/[^ "]+$/,
+  DATE : /\d{4}-\d{2}-\d{2}/
 } as const
 
 export type RegexKey = keyof typeof REGEX
