13486a889a5126cb12608aebd8f1420fa60d442c
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-05 23:11:43 +1000

    [feat-added-config]

--- src/config/dir/path.ts
+++ src/config/dir/path.ts
@@ -6,9 +6,10 @@ export const ID = '/:id'
 export const ROOT = '/'
 
 export const MODULE = {
-  AUTH: 'auth',
-  USER: 'user',
-  GAME: 'game'
+  AUTH  : 'auth',
+  USER  : 'user',
+  GAME  : 'game',
+  SOURCE: 'source'
 }
 
 const AUTH = {
