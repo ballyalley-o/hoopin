ff7861d008cf4ce021c68c30d8bc76e5dbfdcb63
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-05 23:13:36 +1000

    [route]: wired up

--- src/route/index.ts
+++ src/route/index.ts
@@ -1,10 +1,12 @@
 import { Application } from 'express'
 import { linkAuthRoute } from 'route/auth'
 import { linkGameRoute } from 'route/game'
+import { linkSourceRoute } from 'route/source'
 
 export const mainRoute = (app: Application, apiVer: string) => {
     linkAuthRoute(app, apiVer)
     linkGameRoute(app, apiVer)
+    linkSourceRoute(app, apiVer)
 }
 
 export { default as ServerStatic } from './server-static'
