b4d1d9f3ffa57efb206117961e130f6bbde791d8
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-06 18:25:30 +1000

    [fix]: fixed undefined response from outsource

--- src/controller/source.controller.ts
+++ src/controller/source.controller.ts
@@ -1,15 +1,14 @@
 import { NextFunction, Request, Response } from 'express'
-import { CODE, Resp, RESPONSE } from 'constant'
-import { ErrorResponse } from 'middleware'
+import { CODE, Resp } from 'constant'
 import { sourceService } from 'service/source.service'
-
 import { Service } from './service.controller'
 
 const TAG = 'Source.Controller'
 export class SourceController {
     public static async getFreeAgents(_req: Request, res: Response, _next: NextFunction) {
         try {
-            res.status(CODE.OK).json(res.advanceResult)
+            const agents = await sourceService.getFreeAgents()
+            res.status(CODE.OK).json(Resp.Ok(agents, Array.isArray(agents) ? agents.length : undefined))
         } catch (error) {
             Service.catchError(error, TAG, 'getFreeAgents', res)
         }
