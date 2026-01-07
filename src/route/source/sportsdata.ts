f16bfc29596c7dbf612b6994a6b0aaf25c2a033c
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-07 14:18:08 +1000

    [route]: new endpoints

--- src/route/source/sportsdata.ts
+++ src/route/source/sportsdata.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express'
 import { SourceController } from 'controller/source.controller'
+import { asyncHandler } from 'middleware'
 
 const router = Router({ mergeParams: true })
 
 router.get('/freeagent', SourceController.getFreeAgents)
+router.get('/player-active-all', SourceController.getPlayerActiveAll)
+router.get('/player-detail-all', SourceController.getPlayerDetailAll)
+router.get('/player-injured-all', SourceController.getInjuredPlayerAll)
+router.get('/allstar-all', SourceController.getAllStarAll)
 
 export default router
