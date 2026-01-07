8699ad4ad7a69943913bb1f7ce156a10b1dd6b46
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-07 14:19:27 +1000

    [enhance]

--- src/service/source.service.ts
+++ src/service/source.service.ts
@@ -1,7 +1,28 @@
 import axios from 'axios'
 import { SPORTSDATA_DIR, sportsdataHeaders } from 'config/dir/sportsdata'
+import { slugigy } from 'utility'
 
 export const sourceService = {
+  async getPlayerActiveAll() {
+    const url      = SPORTSDATA_DIR.PLAYERS_ACTIVE
+    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    return data
+  },
+  async getPlayerDetailAll() {
+    const url      = SPORTSDATA_DIR.PLAYER_DETAILS
+    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    return data
+  },
+  async getPlayerInjuredAll() {
+    const url      = SPORTSDATA_DIR.INJURED_PLAYERS
+    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    return data
+  },
+  async getAllStarAll(season: string) {
+    const url      = SPORTSDATA_DIR.ALL_STARS(season ?? new Date().getFullYear())
+    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    return data
+  },
   /**
    * Fetch NBA free agents from SportsData.io.
    * Returns the API payload directly.
