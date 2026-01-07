edfdead8b03ffea68c814dfcb87f453c0c2895b5
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-08 02:37:18 +1000

    [route]: endpoints added

--- src/route/source/sportsdata.ts
+++ src/route/source/sportsdata.ts
@@ -4,10 +4,23 @@ import { asyncHandler } from 'middleware'
 
 const router = Router({ mergeParams: true })
 
-router.get('/freeagent', SourceController.getFreeAgents)
 router.get('/player-active-all', SourceController.getPlayerActiveAll)
 router.get('/player-detail-all', SourceController.getPlayerDetailAll)
 router.get('/player-injured-all', SourceController.getInjuredPlayerAll)
+router.get('/player-all-by-team', SourceController.getPlayerAllByTeam)
+
+router.get('/team-all', SourceController.getTeamAll)
+router.get('/team-active-all', SourceController.getTeamActiveAll)
+
+router.get('/score-all-by-date', SourceController.getScoreAllByDate)
+router.get('/score-all-by-date-final', SourceController.getScoreAllByDateFinal)
+
+router.get('/boxscore-all-by-date-final', SourceController.getBoxscoreAllByDateFinal)
+
 router.get('/allstar-all', SourceController.getAllStarAll)
+router.get('/schedule', SourceController.getScheduleAll)
+router.get('/standing', SourceController.getStanding)
+router.get('/news', SourceController.getNewsAll)
+router.get('/freeagent', SourceController.getFreeAgents)
 
 export default router
