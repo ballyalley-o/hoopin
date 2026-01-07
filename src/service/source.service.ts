9c37b7422149fc9ea775f85372c9986fceb36480
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-08 02:37:43 +1000

    [service]: added new services

--- src/service/source.service.ts
+++ src/service/source.service.ts
@@ -1,25 +1,88 @@
 import axios from 'axios'
 import { SPORTSDATA_DIR, sportsdataHeaders } from 'config/dir/sportsdata'
+import type { TeamAbbrNBA } from 'constant'
+
+const _axiosGET = async (url: string) => {
+  const res = await axios.get(url, { headers: sportsdataHeaders() })
+  return res
+}
 
 export const sourceService = {
   async getPlayerActiveAll() {
     const url      = SPORTSDATA_DIR.PLAYERS_ACTIVE
-    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    const { data } = await _axiosGET(url)
     return data
   },
+
   async getPlayerDetailAll() {
     const url      = SPORTSDATA_DIR.PLAYER_DETAILS
-    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    const { data } = await _axiosGET(url)
     return data
   },
+
   async getPlayerInjuredAll() {
     const url      = SPORTSDATA_DIR.INJURED_PLAYERS
-    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    const { data } = await _axiosGET(url)
     return data
   },
+
+  async getPlayerAllByTeam(team: TeamAbbrNBA) {
+    const url      = SPORTSDATA_DIR.PLAYERS_BY_TEAM(team)
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getTeamAll() {
+    const url      = SPORTSDATA_DIR.TEAM_ALL
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getTeamActiveAll() {
+    const url      = SPORTSDATA_DIR.TEAM_ACTIVE
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getScoreAllByDate(date: string) {
+    const url      = SPORTSDATA_DIR.SCORES_BASIC_BY_DATE(date)
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getScoreAllByDateFinal(date: string) {
+    const url      = SPORTSDATA_DIR.SCORES_BASIC_BY_DATE_FINAL(date)
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
   async getAllStarAll(season: string) {
     const url      = SPORTSDATA_DIR.ALL_STARS(season ?? new Date().getFullYear())
-    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getBoxscoreAllByDateFinal(date: string) {
+    const url      = SPORTSDATA_DIR.BOXSCORES_BY_DATE_FINAL(date)
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getScheduleAll(season: string) {
+    const url      = SPORTSDATA_DIR.SCHEDULES(season)
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getStanding(season: string) {
+    const url      = SPORTSDATA_DIR.STANDING(season)
+    const { data } = await _axiosGET(url)
+    return data
+  },
+
+  async getNewsAll() {
+    const url      = SPORTSDATA_DIR.NEWS
+    const { data } = await _axiosGET(url)
     return data
   },
   /**
@@ -28,7 +91,7 @@ export const sourceService = {
    */
   async getFreeAgents() {
     const url      = SPORTSDATA_DIR.PLAYERS_FREEAGENTS
-    const { data } = await axios.get(url, { headers: sportsdataHeaders() })
+    const { data } = await _axiosGET(url)
     return data
   }
 }
