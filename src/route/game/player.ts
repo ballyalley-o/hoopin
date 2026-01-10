12d15c9bd41231e8d4d2b39460508314116cd964
Bally.Lomibao <bally.lomibao@howickltd.com>
2026-01-11 01:04:12 +1000

    [route]: added create-basic route

--- src/route/game/player.ts
+++ src/route/game/player.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { PlayerController } from 'controller'
+import { protect } from 'middleware'
 
 const router = Router({ mergeParams: true })
 
 router.route('/')
   .get(PlayerController.list)
   .post(PlayerController.create)
+
+router.post('/create-basic', protect, PlayerController.createBasic)
 
 router.route('/:id')
   .get(PlayerController.get)
